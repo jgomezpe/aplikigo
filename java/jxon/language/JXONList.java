bc5786855b60ecc14e4f8ad5522549dc0432b356
Jonatan Gomez <jogoper@gmail.com>
2021-02-22 21:53:07 -0500

    Refactor package

--- java/jxon/language/JXONList.java
+++
@@ -1,8 +0,0 @@
-package jxon.language;
-
-import lifya.lookahead.ListRule;
-
-public class JXONList extends ListRule{
-    public final static String TAG = "LIST"; 
-    public JXONList(JXONParser parser) { super(TAG, parser, JXONValue.TAG); }
-}
--- java/jxon/language/JXONObj.java
+++
@@ -1,8 +0,0 @@
-package jxon.language;
-
-import lifya.lookahead.ListRule;
-
-public class JXONObj extends ListRule{
-    public final static String TAG = "OBJ"; 
-    public JXONObj(JXONParser parser) { super(TAG, parser, JXONAttribute.TAG, '{', '}', ','); }
-}
